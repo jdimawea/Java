1b86f286c568aeebc68fa6b5c421ef0eed109a69
Jonathan Dima <dimawea@gmail.com>
2022-02-06 23:03:20 -0700

    commit

---
+++ first_java_program/AboutMe.java
@@ -0,0 +1,15 @@
+// File name must be the same as the class name
+// Every file IS a class
+public class AboutMe {
+
+    // The file or class with the exact method signature below
+    // is always the launching point of the application
+    // You cannot run a Java program without it. 
+
+    public static void main(String[] args) {
+        // For now, all your code goes here
+        System.out.println("My name is Coding Dojo");
+        System.out.println("I am 100 years old");
+        System.out.println("My hometown is Burbank, CA");
+    }
+}
